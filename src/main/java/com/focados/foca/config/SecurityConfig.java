61cb7c97d4161c436f16f1e874126d8aa9779f1e
dante_canal <dantonfilipe.s@gmail.com>
2025-12-05 03:11:35 -0300

    fix: add vercel CORS on SecConfig policy

--- src/main/java/com/focados/foca/config/SecurityConfig.java
+++ src/main/java/com/focados/foca/config/SecurityConfig.java
@@ -51,7 +51,9 @@ public class SecurityConfig {
             "http://localhost:8081",
             "http://localhost:3000",
             "http://127.0.0.1:8081",
-            "http://127.0.0.1:3000"
+            "http://127.0.0.1:3000",
+            "https://foc-aplus-front.vercel.app"
+
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList(
