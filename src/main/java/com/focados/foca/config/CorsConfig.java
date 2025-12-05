cd37385f681f13274e2958f29c15382c4619bf4b
dante_canal <dantonfilipe.s@gmail.com>
2025-12-05 01:54:05 -0300

    fix: add render to CORS policy

--- src/main/java/com/focados/foca/config/CorsConfig.java
+++ src/main/java/com/focados/foca/config/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
                                 "http://localhost:8100",
                                 "http://localhost:8081",
                                 "http://localhost:19006",
-                                "http://127.0.0.1:8081"
+                                "http://127.0.0.1:8081",
+                                "https://focaplus-back.onrender.com"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
