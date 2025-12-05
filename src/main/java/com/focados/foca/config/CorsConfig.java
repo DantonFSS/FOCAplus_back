4168a1e6208ff0164924f6ec172fc877ebb3a45f
dante_canal <dantonfilipe.s@gmail.com>
2025-12-05 01:55:03 -0300

    fix: add vercel to CORS policy

--- src/main/java/com/focados/foca/config/CorsConfig.java
+++ src/main/java/com/focados/foca/config/CorsConfig.java
@@ -18,7 +18,8 @@ public class CorsConfig {
                                 "http://localhost:8081",
                                 "http://localhost:19006",
                                 "http://127.0.0.1:8081",
-                                "https://focaplus-back.onrender.com"
+                                "https://focaplus-back.onrender.com",
+                                "https://foc-aplus-front.vercel.app"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
